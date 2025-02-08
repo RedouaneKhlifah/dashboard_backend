922f72c9216fc705123191f605f18bad6b846d66
RedouaneKhlifahLf <redouane.khlifah@araba.shop>
2025-02-08 07:13:09 +0100

    extend jwt life time to one day

--- config/jwt.php
+++ config/jwt.php
@@ -2,7 +2,7 @@
 
 return [
     'secret' => env('JWT_SECRET'),
-    'ttl' => 60, // Time to live for the token in minutes
+    'ttl' => 1440, // Time to live for the token in minutes
     'refresh_ttl' => 20160, // Refresh token time to live (in minutes)
     'algo' => 'HS256', // Algorithm to use for signing
     'required_claims' => ['sub', 'iat'],
